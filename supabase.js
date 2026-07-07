14692e61e37723e2b7a32d962dcdcd305c5a48b9
Steven Hullings <s.hullings74@gmail.com>
2026-07-07 18:03:48 -0400

    add gitignore to exclude supabase credentials
    Added .gitignore file to prevent supabase.js from being pushed to public GitHub repository. Credentials should be kept local only

--- supabase.js
+++
@@ -1,6 +0,0 @@
-// ========== SUPABASE CONNECTION ==========
-const SUPABASE_URL = 'https://gdoekryeodypttosmdrp.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdkb2Vrcnllb2R5cHR0b3NtZHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNzQwNTEsImV4cCI6MjA5ODc1MDA1MX0.IRTIA0yEBPrmaMFl6Iz46aZfSb2gGtTtxnVP_jm2HTA';
-
-const { createClient } = supabase;
-const supabaseClient = createClient('https://gdoekryeodypttosmdrp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdkb2Vrcnllb2R5cHR0b3NtZHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNzQwNTEsImV4cCI6MjA5ODc1MDA1MX0.IRTIA0yEBPrmaMFl6Iz46aZfSb2gGtTtxnVP_jm2HTA');
